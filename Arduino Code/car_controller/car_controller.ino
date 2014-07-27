1835677b8bd8a630142d7991048e68c5aa9df30c
Kevin J Dolan <git@kevinjdolan.com>
2014-07-27 19:29:46 -0400

    Arduino Changes

--- Arduino Code/car_controller/car_controller.ino
+++ Arduino Code/car_controller/car_controller.ino
@@ -2,6 +2,16 @@
 Adafruit Arduino - Lesson 15. Bi-directional Motor
 */
  
+float MAX_MOTOR_V = 7.0; //Defines the max voltage to be be applied to the motors.
+float MOTOR_BAT_V = 9.2; //Defines the voltage of the battery connected to the L293D
+
+/*
+The max speed value for the motors. This is determined by (MAX_MOTOR_V/MOTOR_BAT_V)*255.
+When the MAX_MOTOR_V is equal to the MOTOR_BAT_V then MAX_ENABLE=255.
+When full throttle is applied to the motor the voltage specified by MAX_MOTOR_V
+will be applied. 
+*/
+int MAX_ENABLE = 0; 
 
 int RF_enbl = 8;
 int RF_in1 = 30;
@@ -23,6 +33,7 @@ void setup()
 {
   Serial.begin(9600);
   
+  MAX_ENABLE = (MAX_MOTOR_V/MOTOR_BAT_V)*255;
   
   pinMode(LF_enbl, OUTPUT);
   pinMode(LF_in1, OUTPUT);
@@ -45,6 +56,16 @@ void setup()
  
 void loop()
 {
+	
+	if(MAX_ENABLE>255)
+	{
+		//Do nothing (Blink LED to indicate problem)
+		digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  		delay(1000);               // wait for a second
+ 		digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  		delay(1000);      
+	}
+		
     Serial.println(y);
     //Wait for pi
     boolean piFound = false;
@@ -74,20 +95,27 @@ void loop()
         index = 0;
         if(cmdChar=='D')
         {          
-           String str_inData = String(inData);
-           String str_LF = str_inData.substring(1,4);
+           /*String str_inData = String(inData);
+           String str_LF = str_inData.substring(1,4); //Will look at characters 2,3,4
            String str_RF = str_inData.substring(4,7);
            String str_LR = str_inData.substring(7,10);
-           String str_RR = str_inData.substring(10,13);
+           String str_RR = str_inData.substring(10,13);*/
+           
            Serial.println();
            //Serial.println(str_RR);
            Serial.println();
            //Serial.println(str_LF);Serial.println(str_RF);Serial.println(str_LR);Serial.println(str_RR);
   
-           int LF = str_LF.toInt();
+           /*int LF = str_LF.toInt();
            int RF = str_RF.toInt();
            int LR = str_LR.toInt();
-           int RR = str_RR.toInt();
+           int RR = str_RR.toInt();*/
+           
+           int LF = (100*(inData[2]-'0'))+(10*(inData[3]-'0'))+((inData[4]-'0'));
+           int RF =
+           int LR = 
+           int RR = 
+           
            //Serial.println(LF);Serial.println(RF);Serial.println(LR);Serial.println(RR);
            //Serial.print("-->");
            //Serial.print(str_inData);
@@ -99,32 +127,36 @@ void loop()
            boolean rev_LR=false;
            boolean rev_RR=false;
            
-           if(LF<100)
-             rev_LF=true;
-           else
-             LF=LF-100;
+           if(LF<100){
+             rev_LF=true;}
+           else{
+             LF=LF-100;}
 
-           if(RF<100)
-             rev_RF=true;
-           else
-             RF=RF-100;
+           if(RF<100){
+             rev_RF=true;}
+           else{
+             RF=RF-100;}
              
-           if(LR<100)
-             rev_LR=true;
-           else
-             LR=LR-100;
+           if(LR<100){
+             rev_LR=true;}
+           else{
+             LR=LR-100;}
              
-            if(RR<100)
-             rev_RR=true;
-           else
-             RR=RR-100;
+           if(RR<100){
+             rev_RR=true;}
+           else{
+             RR=RR-100;}
            
-           LF = ((float)LF/100.0)*255;
-           RF = ((float)RF/100.0)*255;
-           LR = ((float)LR/100.0)*255;
-           RR = ((float)RR/100.0)*255;
+           LF = ((float)LF/100.0)*MAX_ENABLE;
+           RF = ((float)RF/100.0)*MAX_ENABLE;
+           LR = ((float)LR/100.0)*MAX_ENABLE;
+           RR = ((float)RR/100.0)*MAX_ENABLE;
            Serial.println(LF);Serial.println(RF);Serial.println(LR);Serial.println(RR);
-           
+                     
+           setMotor(LF_enbl, LF_in1, LF_in2, LF, rev_LF);
+           setMotor(RF_enbl, RF_in1, RF_in2, RF, rev_RF);
+           setMotor(LR_enbl, LR_in1, LR_in2, LR, rev_LR);
+           setMotor(RR_enbl, RR_in1, RR_in2, RR, rev_RR);
         }
       }
     }
@@ -142,4 +174,5 @@ void setMotor(int enablePin, int in1Pin, int in2Pin, int speed1, boolean reverse
   digitalWrite(in1Pin, ! reverse);
   digitalWrite(in2Pin, reverse);
 }
+
 
